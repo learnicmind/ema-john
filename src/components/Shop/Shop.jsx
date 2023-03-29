62e0d247e53ee40a91eaac0eafc7d3f53f55edf6
Ariful Islam Razu <learnicmind@gmail.com>
2023-03-30 03:12:18 +0600

    retrieve product from stored car

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,10 +15,27 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
-    useEffect( () =>{
+    useEffect( () => {
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    }, [])
+        const savedCart = [];
+
+        // Step 1: get id of the addedProduct
+        for(const id in storedCart){
+            //Step 3:  get product from products state by using id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                // step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // step 4: add the addedProduct to the saved cart
+                savedCart.push(addedProduct)
+            }
+            console.log('added product', addedProduct)
+        }
+        // step 5: set the cart
+        setCart(savedCart)
+
+    }, [products])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
@@ -30,11 +47,11 @@ const Shop = () => {
         <div className='shop-container'>
             <div className="products-container">
                 {
-                    products.map(product => <Product 
+                    products.map(product => <Product
                         key={product.id}
-                        product={product} 
+                        product={product}
                         handleAddToCart={handleAddToCart}
-                        />)
+                    />)
                 }
             </div>
             <div className="cart-container">
