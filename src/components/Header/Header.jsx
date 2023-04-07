66f2e8699c9b99476d8f0a678b1f68918a865604
Ariful Islam Razu <learnicmind@gmail.com>
2023-04-08 00:45:53 +0600

    implemented delete from cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
+import { Link } from 'react-router-dom';
 
 
 
@@ -9,10 +10,10 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div className=''>
-                <a href="/shop">Shop</a>
-                <a href="/order">Order</a>
-                <a href="/inventory">Inventory</a>
-                <a href="/login">Login</a>
+                <Link to="/">Shop</Link>
+                <Link to="/orders">Order</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/login">Login</Link>
             </div>
 
         </nav>
